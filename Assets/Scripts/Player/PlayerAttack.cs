1a7a2c9be9286a53269b0bcaf6f23bfefffe0962
Ryan <ryan.shee@gmail.com>
2017-10-30 19:56:38 -0700

    Player hurtboxes updated, beginning player hitboxes attempt

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
+	public PolygonCollider2D light1, heavy1;
     public PolygonCollider2D[] attackHitboxes;
+    private PolygonCollider2D currentHitbox;
+    public enum hitboxes {
+    	light1Box,
+    	heavy1Box,
+    	clear
+    }
 
 	// Use this for initialization
 	void Start () {
-		
+		attackHitboxes = new PolygonCollider2D[] {
+			light1,
+			heavy1
+		};
+
+		currentHitbox = gameObject.AddComponent<PolygonCollider2D>();
+		currentHitbox.isTrigger = true;
+		currentHitbox.pathCount = 0;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        Animator anim = GetComponent<Animator>();
-        
-    }
+	void OnTriggerEnter2D(Collider2D col) {
+		Debug.Log("Hit something!");
+	}
 
-    private void LaunchAttack(Collider col) {
-
+    public void setHitBox(hitboxes val) {
+    	if (val != hitboxes.clear) {
+    		currentHitbox.SetPath(0, attackHitboxes[(int)val].GetPath(0));
+    		return;
+    	}
+    	currentHitbox.pathCount = 0;
     }
 }
