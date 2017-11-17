1e0d68efcbead25c2111325fd56b74fce7d41dfb
Ryan <ryan.shee@gmail.com>
2017-11-16 17:34:31 -0800

    Vaporwave initial crouching animation added

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -5,16 +5,44 @@ using UnityEngine;
 public class CameraSystem : MonoBehaviour {
     private GameObject[] players;
     public float xMin, yMin, xMax, yMax;
+    // private float margin;
+    // private float z0; // z coordinate of the fighters plane
+    // private float zCam; // camera distance
+    // private float wScene; // scene width
+    // private float xL; // left screen x coordinate
+    // private float xR; // right screen x coordinate
+
+    // void CalcScreen(transform p1, transform p2) {
+    // 	if (p1.position.x < p2.position.x) {
+    // 		xL = p1.position.x - margin;
+    // 		xR = p2.position.x + margin;
+    // 	} else {
+    // 		xL = p2.position.x - margin;
+    // 		xR = p1.position.x + margin;
+    // 	}
+    // }
 
 	// Use this for initialization
 	void Start () {
+		// margin = 1.5f;
         players = GameObject.FindGameObjectsWithTag("Player");
+        // CalcScreen(players[0].transform, players[1].transform);
+        // wScene = xR - xL;
+        // zCam = transform.position.z - z0;
 	}
 	
 	// LateUpdate is called after all Update functions have been called
 	void LateUpdate () {
-        float x = Mathf.Clamp(players[0].transform.position.x, xMin, xMax);
-        float y = Mathf.Clamp(players[0].transform.position.y, yMin, yMax);
-        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+		// CalcScreen(players[0].transform, players[1].transform);
+		// float width = xR - xL;
+		// if (width > wScene) {
+		// 	gameObject.transform.position.x = zCam * width / wScene + z0;
+		// }
+		// gameObject.transform.position.x = (xR + xL) / 2;
+        float x0 = Mathf.Clamp(players[0].transform.position.x, xMin, xMax);
+        float y0 = Mathf.Clamp(players[0].transform.position.y, yMin, yMax);
+        float x1 = Mathf.Clamp(players[1].transform.position.x, xMin, xMax);
+        float y1 = Mathf.Clamp(players[1].transform.position.y, xMin, xMax);
+        gameObject.transform.position = new Vector3((x0 + x1) / 2, (y0 + y1) / 2, gameObject.transform.position.z);
     }
 }
